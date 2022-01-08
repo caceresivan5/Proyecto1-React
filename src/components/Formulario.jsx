0f68c30880ba2d469f9cd04be20de036ead2ee90
Ivan Caceres <dg2.ivancaceres@gmail.com>
2022-01-08 19:43:00 -0300

    form label

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -7,26 +7,29 @@ const Formulario = () => {
             <h1 className="Formulario-titulo ">SEGUIMIENTO PRODUCCION</h1>
             <p>INGRESE LOS CORTES</p>
 
-            <form>
-                <div>
+            <form className='form'>
+                <div className='form-body'>
+
+                <div className='form-Ingreso'>
                     <label>Fecha</label>
                     <input type="data" placeholder='Fecha de Ingreso'></input>
                 </div>
-                <div>
+                <div className='form-Ingreso'>
                     <label>Orden de Corte</label>
                     <input type="number" placeholder='Numero de Corte'></input>
                 </div>
-                <div>
+                <div className='form-Ingreso'>
                     <label>Articulo</label>
                     <input type="text" placeholder='Articulo'></input>
                 </div>
-                <div>
+                <div className='form-Ingreso'>
                     <label>Parte Estampada</label>
                     <input type="text" placeholder='Piezas a Estampar'></input>
                 </div>
-                <div>
+                <div className='form-Ingreso'>
                     <label>Cantidad Total</label>
                     <input type="number" placeholder='Cantidad Total'></input>
+                </div>
                 </div>
             </form>
         </div>
