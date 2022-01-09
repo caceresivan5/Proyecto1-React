ba2ee5949e5853bec6bf571e8e6f2b1f02055685
Ivan Caceres <dg2.ivancaceres@gmail.com>
2022-01-09 19:27:52 -0300

    finalizando style del form

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,32 +4,33 @@ import "../styles/formulario.css"
 const Formulario = () => {
     return (
         <div >
-            <h1 className="Formulario-titulo ">SEGUIMIENTO PRODUCCION</h1>
-            <p>INGRESE LOS CORTES</p>
+            <h1 className="Formulario-titulo ">FORMULARIO DE INGRESO</h1>
+            <p>DATOS DE LOS CORTES</p>
 
             <form className='form'>
                 <div className='form-body'>
 
                 <div className='form-Ingreso'>
-                    <label>Fecha</label>
-                    <input type="data" placeholder='Fecha de Ingreso'></input>
+                    <label htmlFor='fecha'>Fecha</label>
+                    <input id='fecha' type="date" placeholder='Fecha de Ingreso'></input>
                 </div>
                 <div className='form-Ingreso'>
-                    <label>Orden de Corte</label>
-                    <input type="number" placeholder='Numero de Corte'></input>
+                    <label htmlFor='ordenCorte' >Orden de Corte</label>
+                    <input id='ordenCorte' type="number" placeholder='Numero de Corte'></input>
                 </div>
                 <div className='form-Ingreso'>
-                    <label>Articulo</label>
-                    <input type="text" placeholder='Articulo'></input>
+                    <label htmlFor='cliente'>Cliente</label>
+                    <input id='cliente' type="text" placeholder='Cliente'></input>
                 </div>
                 <div className='form-Ingreso'>
-                    <label>Parte Estampada</label>
-                    <input type="text" placeholder='Parte a Estampar'></input>
+                    <label htmlFor='articulo'>Articulo</label>
+                    <input  id='articulo' type="text" placeholder='Articulo'></input>
                 </div>
                 <div className='form-Ingreso'>
-                    <label>Cantidad Total</label>
-                    <input type="number" placeholder='Cantidad Total'></input>
+                    <label htmlFor='cantidad'>Cantidad Total</label>
+                    <input id='cantidad' type="number" placeholder='Cantidad Total'></input>
                 </div>
+                <input className='Btn-enviar' type="submit" />
                 </div>
             </form>
         </div>
